17749748d30ac3c6c8b4eb3030cf316b5b55e024
Osman Ugur Demirer <osmanugurdemirer@outlook.com>
2023-02-05 02:17:53 +0300

    pokes are appended to mongodb

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,29 @@
+from pymongo import MongoClient
+
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from typing import List, NamedTuple
+username = "deneme"
+password = "password_HERE"
+client = MongoClient(f'mongodb+srv://deneme:(password_Here)@cluster0.zz9wxyc.mongodb.net/?retryWrites=true&w=majority')
+db = client.ugurdmrer
+pokemonCollection = db.pokemon
+
+
+scrapedPokeData = []
+class Pokemon(NamedTuple):
+    poke_id: int
+    pokeName: str
+    pokeUrl: str
+    pokeImage: str
+    healthPower:int
+    attackPower: int
+    attackPower: int
+    defencePower: int
+    specialAttack: int
+    specialDefence: int
+    speed: int
+    pokeTypes: List[str]
 
 # Configure
 url = 'https://pokemondb.net/pokedex/all'
@@ -40,10 +64,40 @@ for pokemon in pokemonRows:
     for types in pokemonData[2].find_all("a"):
         pokeTypes.append(types.getText())
     
-    entryUrl = f'https://pokemondb.net{pokeUrl}'
-    request =Request(
-        entryUrl,
-        headers={'User-Agent': 'Mozilla/5.0'}
+    # entryUrl = f'https://pokemondb.net{pokeUrl}'
+    # request =Request(
+    #     entryUrl,
+    #     headers={'User-Agent': 'Mozilla/5.0'}
+    # )
+    # entryPageHtml = urlopen(request).read().decode('utf-8')
+    # entrySoup = BeautifulSoup(entryPageHtml, "html.parser")
+    
+    typedPokemon = Pokemon(
+        poke_id = int(poke_id),
+        pokeName = str(pokeName),
+        pokeUrl = pokeUrl,
+        pokeImage = pokeImage,
+        healthPower = int(healthPower),
+        attackPower= int(attackPower),
+        defencePower= int(defencePower),
+        specialAttack=int(specialAttack),
+        specialDefence=int(specialDefence),
+        speed=int(speed),
+        pokeTypes=pokeTypes
     )
-    entryPageHtml = urlopen(request).read().decode('utf-8')
-    entrySoup = BeautifulSoup(entryPageHtml, "html.parser")
+    scrapedPokeData.append(typedPokemon)
+    
+    
+    pokemonCollection.insert_one(
+        {
+            "id": typedPokemon.poke_id,
+            "name": typedPokemon.pokeName,
+            "image":typedPokemon.pokeImage,
+            "types": typedPokemon.pokeTypes,
+            "hp": typedPokemon.healthPower,
+            "defense": typedPokemon.defencePower,
+            "sp_attack": typedPokemon.specialAttack,
+            "sp_defense": typedPokemon.specialDefence,
+            "speed": typedPokemon.speed,
+        }
+    )
